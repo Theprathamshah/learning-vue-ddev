71f1c339f58481540d30d6f2b3f86cfa86d0060a
Pratham Shah <itspratham2911@gmail.com>
2024-06-20 16:58:22 +0530

    Added mark todo as completed and delete todo feture to the application

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -5,27 +5,49 @@ const header = ref('To Do List')
 const newTodo = ref("")
 const priority = ref(false)
 const items = ref([
-    { text: "Do leetcode problem", completed: false },
-    { text: "Learn caro kann theory", completed: true },
-    { text: "Study about backend design patterns", completed: false }
+    { id: 1, text: "Do leetcode problem", isCompleted: true, isHighPriority: true },
+    { id: 2, text: "Learn caro kann theory", isCompleted: false, isHighPriority: false },
+    { id: 3, text: "Study about backend design patterns", isCompleted: true, isHighPriority: true }
 ]);
+
+
+function toggleCompleted(todo) {
+    todo.isCompleted = !todo.isCompleted;
+}
+
+function deleteTodo(todo) {
+    items.value = items.value.filter((item) => item.id !== todo.id);
+}
 
 </script>
 
 <template>
     <h1>{{ header }}</h1>
-    <form class="add-item-form" @submit.prevent ="items.push({id:items.length,text:newTodo,completed:false,isHighPriority:priority})">
+    <div class="add-item-form">
 
-        <input v-model="newTodo"  placeholder="Add new Todo" type="text" >
-        <label >
+        <input v-model="newTodo"
+            v-on:keyup.enter="items.push({ text: newTodo, isCompleted: false, isHighPriority: false, id: items.length + 1 })"
+            placeholder="Add new Todo" type="text">
+        <label>
             <input type="checkbox" name="" v-model="priority" id="">
             High Priority
         </label>
-        <button class="btn btn-primary" >Add Item</button>
-    </form>
+        <button class="btn btn-primary"
+            v-on:click="items.push({ text: newTodo, isCompleted: false, isHighPriority: false, id: items.length + 1 })">Add
+            Item</button>
+    </div>
     <ul>
-        <li v-for="item in items">{{ item.text }}  <span style="color: red"> {{ item.isHighPriority ? 'High Priority' : '' }}</span></li>
+        <li v-for="item in items"  >
+            <span :class="{ completed: item.isCompleted }" @click="toggleCompleted(item)">{{ item.text }} </span> 
+            <button style="color: red"  @click="deleteTodo(item)">Delete</button>
+        </li>
     </ul>
 
 
 </template>
+
+<style>
+.completed {
+    text-decoration: line-through;
+}
+</style>
